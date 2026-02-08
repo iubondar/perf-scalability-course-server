5f75ff552e99069fb4931ddfdba10718180b45ea
Ivan Bondar <iubondar@gmail.com>
2026-02-08 20:25:00 +0300

    Add payload handler from example

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,14 +2,20 @@ package router
 
 import (
 	"github.com/go-chi/chi"
-	"github.com/iubondar/perf-scalability-cource-server/internal/handler"
+	"github.com/iubondar/perf-scalability-cource-server/internal/handlers"
+	"github.com/iubondar/perf-scalability-cource-server/internal/payload"
 )
 
 func NewRouter() (chi.Router, error) {
 	router := chi.NewRouter()
 
-	helloWorldHandler := handler.NewHelloWorldHandler()
+	helloWorldHandler := handlers.NewHelloWorldHandler()
 	router.Get("/hello", helloWorldHandler.Handle)
+
+	// payload
+	cpuSleep := payload.NewGetrusagePayload()
+	ioSleep := payload.NewIOPayload()
+	router.Get("/payload", handlers.SleepHandler(cpuSleep, ioSleep))
 
 	return router, nil
 }
